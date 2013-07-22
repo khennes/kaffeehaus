c72f1752d218772ee87d142029b2822369b46c3a
Katherine Hennes <katherine.hennes@gmail.com>
2013-07-22 15:24:55 -0700

    Pause for victory lap aka retab

--- lexer.py
+++ lexer.py
@@ -3,60 +3,60 @@ import ply.lex as lex
 
 # List of token names. This is always required.
 tokens = (
-	'NUMBER',
-	'GLOBAL',
-	'DEFCONST',
-	'INCLUDE',
-	'COMMENT',
-	'ID',
+    'NUMBER',
+    'GLOBAL',
+    'DEFCONST',
+    'INCLUDE',
+    'COMMENT',
+    'ID',
 
-	# Operators
-	'PLUS',
-	'MINUS',
-	'TIMES',
-	'DIVIDE',
-	'POWER',
-	'MODULO',
-	'INCREMENT',
-	'DECREMENT',
-	'EQUALS',
-	'ISEQ',
-	'ISNOTEQ',
-	'GREATER',
-	'LESS',
-	'LESSEQ',
-	'GREATEQ',
-	'BOOLAND',
-	'BOOLOR',
+    # Operators
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'POWER',
+    'MODULO',
+    'INCREMENT',
+    'DECREMENT',
+    'EQUALS',
+    'ISEQ',
+    'ISNOTEQ',
+    'GREATER',
+    'LESS',
+    'LESSEQ',
+    'GREATEQ',
+    'BOOLAND',
+    'BOOLOR',
 
-	# Delimiters
-	'LBRACK',
-	'RBRACK',
-	'LBRACE',
-	'RBRACE',
-	'LPAREN',
-	'RPAREN'
+    # Delimiters
+    'LBRACK',
+    'RBRACK',
+    'LBRACE',
+    'RBRACE',
+    'LPAREN',
+    'RPAREN'
 )
 
 # Store reserved keywords in Python dictionary
 reserved = { 'if' : 'IF',
-	'then' : 'THEN',
-	'else' : 'ELSE',
-	'elsif' : 'ELSIF',
-	'while' : 'WHILE',
-	'for' : 'FOR',
-	'def' : 'DEF',
-	'bool' : 'BOOL',
-	'int' : 'INT',
-	'float' : 'FLOAT',
-	'struct' : 'STRUCT',
-	'char' : 'CHAR', 
-	'array' : 'ARRAY',
-	'break' : 'BREAK',
-	'continue' : 'CONTINUE',
-	'return' : 'RETURN',
-	'get' : 'GET',  
-	'print' : 'PRINT'
+    'then' : 'THEN',
+    'else' : 'ELSE',
+    'elsif' : 'ELSIF',
+    'while' : 'WHILE',
+    'for' : 'FOR',
+    'def' : 'DEF',
+    'bool' : 'BOOL',
+    'int' : 'INT',
+    'float' : 'FLOAT',
+    'struct' : 'STRUCT',
+    'char' : 'CHAR', 
+    'array' : 'ARRAY',
+    'break' : 'BREAK',
+    'continue' : 'CONTINUE',
+    'return' : 'RETURN',
+    'get' : 'GET',  
+    'print' : 'PRINT'
 }
 
 # Build list of tokens + reserved keywords
@@ -65,33 +65,33 @@ tokens = [ 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'POWER', 'MODULO', 'INC
 
 # Regular expression rules for simple tokens
 # Python raw strings are used for convenience
-t_PLUS 		= r'\+'
-t_MINUS		= r'-'
-t_TIMES		= r'\*'
-t_DIVIDE	= r'/'
-t_POWER		= r'\*\*'
-t_MODULO	= r'%'
-t_INCREMENT	= r'\+\+'
-t_DECREMENT	= r'--'
-t_EQUALS	= r'='
-t_ISEQ		= r'\=\='
-t_ISNOTEQ	= r'!='
-t_GREATER	= r'>'
-t_LESS		= r'<'
-t_LESSEQ	= r'<='
-t_GREATEQ	= r'>='
-t_BOOLAND	= r'&&'
-t_BOOLOR	= r'\|\|'
-t_LBRACK	= r'\['
-t_RBRACK	= r'\]'
-t_LBRACE	= r'{'
-t_RBRACE	= r'}'
-t_LPAREN	= r'\('
-t_RPAREN	= r'\)'
-t_GLOBAL	= r'\$'
+t_PLUS	    = r'\+'
+t_MINUS	    = r'-'
+t_TIMES	    = r'\*'
+t_DIVIDE    = r'/'
+t_POWER	    = r'\*\*'
+t_MODULO    = r'%'
+t_INCREMENT = r'\+\+'
+t_DECREMENT = r'--'
+t_EQUALS    = r'='
+t_ISEQ	    = r'\=\='
+t_ISNOTEQ   = r'!='
+t_GREATER   = r'>'
+t_LESS	    = r'<'
+t_LESSEQ    = r'<='
+t_GREATEQ   = r'>='
+t_BOOLAND   = r'&&'
+t_BOOLOR    = r'\|\|'
+t_LBRACK    = r'\['
+t_RBRACK    = r'\]'
+t_LBRACE    = r'{'
+t_RBRACE    = r'}'
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_GLOBAL    = r'\$'
 # DEFCONST, INCLUDE, ID  # can mean include, define (constant), or inline comment
-# t_STCOMM	= r'\/\*\*\*'  # How to handle multiline comments?
-# t_ENDCOMM	= r'\*\*\*\/'
+# t_STCOMM  = r'\/\*\*\*'  # How to handle multiline comments?
+# t_ENDCOMM = r'\*\*\*\/'
 
 """
 If some action needs to be taken, define rules by functions.
@@ -105,34 +105,34 @@ LexToken attributes:
 """
 
 # A string containing ignored characters (spaces, tabs)
-t_ignore	= ' \t'
+t_ignore = ' \t'
 
 def t_NUMBER(t):
-	r'\d+' 
-	t.value = int(t.value)
-	return t
+    r'\d+' 
+    t.value = int(t.value)
+    return t
 
 # Track line numbers
 def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
 # Recognize, discard comments
 def t_COMMENT(t):
-	r'\#.*'  # Should this be r'\#*'? How to handle multi-line comments?
-	pass
-	# no return value, token discarded
+    r'\#.*'  # Should this be r'\#*'? How to handle multi-line comments?
+    pass
+    # no return value, token discarded
 
 # Store IDs in a dictionary?
 def t_ID(t):
-	r'[a-zA-Z_][a-zA-Z_0-9]*'
-	t.type = reserved.get(t.value, 'ID')  # Check for reserved words
-	return t
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
+    return t
 
 # Error handling rule
 def t_error(t):
-	print "Invalid character: '%s'" % t.value[0]
-	t.lexer.skip(1)
+    print "Invalid character: '%s'" % t.value[0]
+    t.lexer.skip(1)
 
 # Build the lexer
 lexer = lex.lex()
@@ -145,8 +145,8 @@ lexer.input(f)
 
 # Tokenize
 while True:
-	tokens = lexer.token()
-	if not tokens: 
-		break  # No more input
-	print tokens
+    tokens = lexer.token()
+    if not tokens: 
+	break  # No more input
+    print tokens
 
