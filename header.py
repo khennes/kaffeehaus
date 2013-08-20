575af66e4abf1a0bba7ec8f4f2596419ac7b917d
Katherine Hennes <katherine.hennes@gmail.com>
2013-08-19 19:54:55 -0700

    Update .js template, remove footer, mark up HTML

--- header.py
+++ header.py
@@ -1,23 +1,60 @@
-""" Header file for compiled asm.js files """
+""" Header/footer for compiled .js files """
 
 """ Typed arrays split implementation into a buffer [ArrayBuffer class] and a
 view [ArrayBufferView class]."""
 
-def header():
-    x = '''
-        var buffer = new ArrayBuffer(24);  # why 24?
+TEMPLATE = '''\
+main = function() {
+var heap = new ArrayBuffer(512);
 
-        var Int8Array = new ArrayBufferView(buffer, 0, 0);  // data type, starting offset, # elements
-        var Uint8Array = new ArrayBufferView(buffer, 
-        var Int16Array = new ArrayBufferView(buffer, 
-        var Uint16Array = new ArrayBufferView(buffer,
-        var Int32Array = new ArrayBufferView(buffer,
-        var Uint32Array = new ArrayBufferView(buffer,
-        var Float32Array = new ArrayBufferView(buffer,
-        var Float64Array = new ArrayBufferView(buffer,
-    '''
-    return x
+function Module(stdlib, foreign, heap) {
+    "use asm";
+
+    <!-- Globals -->
+    var I8 = new stdlib.Int8Array(heap);
+    var U8 = new stdlib.Uint8Array(heap);
+    var 16I = new stdlib.Int16Array(heap);
+    var 16U = new stdlib.Uint16Array(heap);
+    var I32 = new stdlib.Int32Array(heap);
+    var U32 = new stdlib.Uint32Array(heap);
+
+    var log = foreign.print;
+
+    <!-- Functions -->
+    
+    %s
+
+    <!-- End Functions -->
+    
+    <!-- Pointers to heap access -->
+
+    %s;
+
+    <!-- Exporting compiled functions -->
+
+    %s;
+}
+
+var print = function(input) {
+    document.getElementById("compilerOutput").innerHTML += input;
+}
+
+<!-- Revert strings to characters -->
+
+for (i = 0; i <= heap; i++) {
+    while (heap[i] != "\0") {
+        heap[i] = String.fromCharCode(heap[i])
+        print heap[i]
+        }
+    print heap[i]
+}
 
 
+var foreign = {"print": print};
 
+var asmjs = Module(window, foreign, heap);
+
+}
+main();
+'''
 
